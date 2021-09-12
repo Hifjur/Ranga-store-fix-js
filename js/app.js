043e9f8674a4622a91fa3a477b58a27fd3678080
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-09-12 23:55:11 +0600

    fixed cart price and total

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -48,7 +48,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = Math.round(total*100)/100;
 };
 
 // set innerText function
@@ -79,5 +79,5 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = Math.round(grandTotal*100)/100;
 };
